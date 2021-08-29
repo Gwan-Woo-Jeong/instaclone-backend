0fe2c14f6e8923fe09eb6bd6059d5cbb1e7596bd
Gwan-Woo-Jeong <gwanuuoo@gmail.com>
2021-08-29 23:01:34 +0900

    uploadPhoto finished

---
+++ src/photos/photos.typeDefs.ts
@@ -0,0 +1,20 @@
+import { gql } from "apollo-server";
+
+export default gql`
+  type Photo {
+    id: String!
+    user: User!
+    file: String!
+    caption: String
+    hashtag: [Hashtag]
+    createAt: String!
+    updatedAt: String!
+  }
+  type Hashtag {
+    id: String!
+    hashtag: String!
+    photos: [Photo]
+    createAt: String!
+    updatedAt: String!
+  }
+`;
---
+++ src/photos/uploadPhoto/uploadPhoto.resolvers.ts
@@ -0,0 +1,38 @@
+import client from "../../client";
+import { Resolvers } from "../../types";
+import { protectedResolver } from "../../users/users.utils";
+
+const resolvers: Resolvers = {
+  Mutation: {
+    uploadPhoto: protectedResolver(
+      async (_, { file, caption }, { loggedInUser }) => {
+        let hashtagObjs = [];
+        if (caption) {
+          const hashtags = caption.match(/#[ㄱ-ㅎ|ㅏ-ㅣ|가-힣|\w]+/g);
+          hashtagObjs = hashtags.map((hashtag) => ({
+            where: { hashtag },
+            create: { hashtag },
+          }));
+        }
+        return client.photo.create({
+          data: {
+            file,
+            caption,
+            user: {
+              connect: {
+                id: loggedInUser.id,
+              },
+            },
+            ...(hashtagObjs.length > 0 && {
+              hashtags: {
+                connectOrCreate: hashtagObjs,
+              },
+            }),
+          },
+        });
+      }
+    ),
+  },
+};
+
+export default resolvers;
